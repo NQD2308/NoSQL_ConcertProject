b9bce6d049c1147717ae662fbecb1e0cf7762a7f
Đinh Quốc Cường <tr6r20@gmail.com>
2024-06-22 07:30:20 +0700

    update find one

--- concert.py
+++ concert.py
@@ -11,7 +11,19 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 #     print("Pinged your deployment. You successfully connected to MongoDB!")
 # except Exception as e:
 #     print(e)
+def signin(user,password):
+    db = client['ManageAsset']
 
+# Access the collection (assuming the collection name is "Concert")
+    concert_collection = db['User']
+
+    # Find one document with the specified criteria
+    result = concert_collection.find_one({"name": user, "password": password})
+    print(result)
+    if result is not None:
+        print("Đăng nhập thanh cong")
+    else:
+        print("Đăng nhập khong thanh cong")
 
 def main():
     print("1: Đăng nhập")
@@ -21,9 +33,14 @@ def main():
         case 1:
             print("Đăng Nhập")
             username = input("username: ")
+            password = input("password: ")
+            signin(username,password)
 
         case 2:
             print("Đăng ký")
+            username = input("username: ")
+            password = input("password: ")
+            signin(username,password)
     
 if __name__ == '__main__':
     main()
